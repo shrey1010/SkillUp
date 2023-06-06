9fe29d7aacf7fb10cfe7107df4de81181c9a00e8
Shrey <shreyshukla1010@gmail.com>
2023-06-06 15:47:21 +0530

    fix : Bug Fix

--- SkillUp/courses/views/homepage.py
+++ SkillUp/courses/views/homepage.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
-# Create your views here.
+from courses.models import Course
+from django.views.generic import ListView
 
-def home(request):
-    return render(request, "courses/home.html")
+
+class HomePageView(ListView):
+    template_name = "courses/home.html"
+    queryset = Course.objects.filter(active=True)
+    context_object_name = 'courses'
